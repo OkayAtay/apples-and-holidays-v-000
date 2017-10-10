9336c03a0d0629545dc051b1e171825c491de03e
Araba Taylor <araba.taylor20@gmail.com>
2017-10-10 23:23:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   supply_array.flatten
-  holiday_hash[:season][:holiday_name]<<supply_array
+  holiday_hash[:season][:holiday_name]<<supply_array.flatten
 
   # code here
   # remember to return the updated hash
